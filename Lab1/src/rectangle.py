695440a4d1561a24a6a2891e11fb6e122803a954
但庆娟 <danqingjuan@Qingjuans-MacBook-Pro.local>
2023-09-14 19:06:10 -0700

    add the shape class

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,21 +5,32 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+class Shape(ABC):
+	def __init__(self, width, height):
+		self._width = width
+		self._height = height
+		
+	@abstractmethod
+	def set_values(self, width, height):
+		pass
 
-    def area(self):
-        return self.width * self.height
+	@abstractmethod
+	def area(self):
+		pass
+class Rectangle:
+	def set_values(self, x, y):
+		super().__init__(width, height)
+
+    	def area(self):
+        	return self.width * self.height
 
 
 if __name__ == "__main__":
-    # Create a rectangle object
-    rect = Rectangle()
+   	 # Create a rectangle object
+    	rect = Rectangle()
 
-    # Call a member function
-    rect.set_values(3, 4)
+    	# Call a member function
+    	rect.set_values(3, 4)
 
-    # Print out the area function
-    print("area:", rect.area())
+    	# Print out the area function
+    	print("area:", rect.area())
